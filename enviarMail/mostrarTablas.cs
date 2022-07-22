33080cf86fdae9bc33af857ab5673a79cbde454a
hiram.ramirezg <hiram.ramirezg@DESKTOP-AS3SF3V>
2022-07-22 11:33:31 -0500

    Agregar archivos de proyecto.

---
+++ enviarMail/mostrarTablas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enviarMail
+{
+    public class mostrarTablas
+    {
+        SqlConnection conn = new SqlConnection("Data Source = 148.234.13.218; Initial Catalog = PruebasSoporteDesarrollo; Integrated Security = True ");
+        DataTable Dtt;
+
+        public DataTable Listar()
+        {
+            Dtt = new DataTable();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM INFORMATION_SCHEMA.TABLES  ", conn);
+                cmd.CommandType = CommandType.Text;
+                conn.Open();
+
+                SqlDataAdapter da = new SqlDataAdapter();
+
+                da.SelectCommand = cmd;
+
+                da.Fill(Dtt);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return Dtt;
+        }
+    }
+}
---
+++ enviarMail/obtenerTablas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace enviarMail
+{
+    public class obtenerTablas
+    {
+        public DataTable Bases()
+        {
+            string[] BD = new string[] { "PruebasSoporteDesarrollo", "CapacitacionVer2", "CapacitacionVer3" };
+
+            DataTable bases;
+            bases = new DataTable();
+
+            foreach (var item in BD)
+            {
+
+                SqlConnection conn = new SqlConnection($"Data Source = 148.234.13.218; Initial Catalog = {item}; Integrated Security = True ");
+
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM INFORMATION_SCHEMA.TABLES ", conn);
+                    cmd.CommandType = CommandType.Text;
+                    conn.Open();
+
+                    SqlDataAdapter da = new SqlDataAdapter();
+
+                    da.SelectCommand = cmd;
+
+                    da.Fill(bases);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+
+
+            }
+
+            return Dtt;
+        }
+
+
+    }
+}
